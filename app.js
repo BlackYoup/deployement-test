64bcaab3489b854d93dbec82feb388a2c81f68b9
BlackYoup <a.lefebvre@outlook.fr>
2015-01-13 18:11:53 +0100

    Add color debug

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+process.env.DEBUG = "*";
+
 var express = require('express');
+var debug = require('debug')('App');
+var myModule = require('./module.js');
 
 var app = express();
 
 setInterval(function(){
-    console.log('LOG');
-}, 1000);
+    debug('Youhavesomushswagonu');
+}, 5000);
 
-app.listen(8080, function(){
-    console.log('server is listening');
+app.listen(8081, function(){
+    require('debug')('server')('server is listening');
 });
---
+++ module.js
@@ -0,0 +1,5 @@
+var debug = require('debug')('module');
+
+setInterval(function(){
+  debug('Module is doing something HAHAHAHA');
+}, 1000);
