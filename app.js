499b9b2fbb900fe814681d42b77d73465b6838f9
BlackYoup <a.lefebvre@outlook.fr>
2015-01-13 15:04:55 +0100

    Add a log line

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express');
 
 var app = express();
 
+setInterval(function(){
+    console.log('LOG');
+}, 1000);
+
 app.listen(8080, function(){
     console.log('server is listening');
 });
