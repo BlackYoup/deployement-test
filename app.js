faee0b7f4f5e624be36b5a6ca78383a82b04e60c
BlackYoup <a.lefebvre@outlook.fr>
2015-01-14 10:41:07 +0100

    Tests multi lines colors

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-var color = require('colors');
 var express = require('express');
 var myModule = require('./module.js');
 
 var app = express();
 
 setInterval(function(){
-    console.log('Youhavesomushswagonu'.red);
+    console.log('\u001b[31mLine1');
+    console.log('line 2');
+    console.log('line 3');
+    console.log('line 4');
+    console.log('line 5\u001b[39m');
+    console.log('line6');
 }, 5000);
 
 app.listen(8080, function(){
-    console.log('server is listening'.yellow);
 });
--- module.js
+++ module.js
@@ -1,5 +1,8 @@
-var color = require('colors');
-
 setInterval(function(){
-  console.log('Module is doing something HAHAHAHA'.blue);
-}, 1000);
+  console.log('\u001b[36mmodule line1');
+  console.log('module line 2');
+  console.log('module line 3');
+  console.log('module line 4');
+  console.log('module line 5\u001b[39m');
+  console.log('module line6');
+}, 3000);
