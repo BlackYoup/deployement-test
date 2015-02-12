f06dc0f47d8a4416d6e94f00fd0f61352d73da00
BlackYoup <a.lefebvre@outlook.fr>
2015-02-12 17:49:38 +0100

    Much messages

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var express = require('express');
 var app = express();
 
 setInterval(function(){
-    console.log(new Date().toISOString());
-}, 2000);
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+}, 1000);
 
 app.listen(8080);
