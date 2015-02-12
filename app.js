c4e4ea6f5ccfa61c358322f513eea935c589bb87
BlackYoup <a.lefebvre@outlook.fr>
2015-02-12 11:20:20 +0100

    Update

--- app.js
+++ app.js
@@ -1,16 +1,8 @@
 var express = require('express');
-var myModule = require('./module.js');
-
 var app = express();
 
 setInterval(function(){
-    console.log('\u001b[31mLine1');
-    console.log('line 2');
-    console.log('line 3');
-    console.log('line 4');
-    console.log('line 5\u001b[39m');
-    console.log('line6');
-}, 5000);
+    console.log(new Date().toISOString());
+}, 2000);
 
-app.listen(8080, function(){
-});
+app.listen(8080);
