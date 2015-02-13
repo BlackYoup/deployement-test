1c9a559314117fde378eb9c706e09dc2788c564c
BlackYoup <a.lefebvre@outlook.fr>
2015-02-13 13:35:00 +0100

    ToISOString()

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ var express = require('express');
 var app = express();
 
 setInterval(function(){
-    console.log(1);
-    console.log(2);
-    console.log(3);
-    console.log(4);
+    console.log(new Date().toISOString());
 }, 1000);
 
 app.listen(8080);
