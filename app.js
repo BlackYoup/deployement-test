812e733025588345d727e3f148605455f04f7339
BlackYoup <a.lefebvre@outlook.fr>
2015-11-17 18:36:19 +0100

    Swag

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 setInterval(function(){
-    console.log("<somehtml yolo>");
+    console.log("<somehtml yoloswag>");
 }, 5001);
 
 app.listen(8080);
