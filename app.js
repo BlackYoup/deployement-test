710a9e48e7fa89716ac04a9ba33d510c797268e6
BlackYoup <a.lefebvre@outlook.fr>
2016-11-28 17:52:23 +0100

    print headers

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ setInterval(function(){
 
 app.listen(8080);
 
+app.use(function(req, res, next) {
+  console.log("HEADERS", req.headers);
+  var proto = req.headers["x-forwarded-proto"];
+
+  if(proto && proto != "https") {
+    res.redirect("https://" + req.headers.host + req.url);
+  }
+  else {
+    next();
+  }
+});
+
 app.get('/', function(req, res){
     res.send('Hello, second version !');
 });
