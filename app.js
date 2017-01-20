33194b43ef62ff07d23c302807e7eb63edf7564b
BlackYoup <a.lefebvre@outlook.fr>
2017-01-20 10:58:49 +0100

    utf-8

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 setInterval(function(){
-  console.log(`Instance number ${process.env.INSTANCE_NUMBER}`);
+  console.log(`Instance number ${process.env.INSTANCE_NUMBER}: éêçâüï`);
 }, 5000);
 
 app.listen(8080);
