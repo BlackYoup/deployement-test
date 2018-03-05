e1d87eda81ef4bab3891fc20af6a880602f1eb1d
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2018-03-05 19:19:24 +0100

    timeout

--- app.js
+++ app.js
@@ -1,75 +1,12 @@
 var express = require('express');
-var metrics = require('express-metrics');
 var app = express();
-var fs = require("fs");
-
-process.exit(1);
-
-app.use(metrics({
-  port: 8081,
-  statsd: {
-    host: '127.0.0.1',
-    port: 8125,
-    prefix: 'my-app',
-    routes: {
-      getSlash: [{ path: '/', methods: ['get'] }]
-    }
-  }
-}));
-
-//setInterval(function(){
-//  console.log(`Instance number ${process.env.INSTANCE_NUMBER}`, (new Date()).toUTCString());
-//}, 5000);
-
-function leak() {
-  function LeakingClass() {
-  }
-
-  var leaks = [];
-  setInterval(function() {
-    for (var i = 0; i < 100000; i++) {
-      leaks.push(new LeakingClass);
-    }
-
-    console.error('Leaks: %d', leaks.length);
-  }, 10);
-}
-
-app.listen(process.env.PORT || 8080);
-
-app.use(function(req, res, next) {
-  console.log("HEADERS", req.headers);
-  var proto = req.headers["x-forwarded-proto"];
-  console.log("proto", proto);
-
-  if(proto && proto != "https") {
-    res.redirect("https://" + req.headers.host + req.url);
-  }
-  else {
-    next();
-  }
-});
 
 app.get('/', function(req, res){
-    res.send('Hello, second version !');
+  res.send('Hello, second version !');
 });
 
-app.get('/network', function(req, res) {
-  fs.createReadStream("/dev/urandom").pipe(res);
-});
+//app.listen(process.env.PORT);
 
-app.get('/test', function(req, res){
-  res.send('heavy task starting..');
-
-  for(var i = 0; i < 10000000000; i++){
-    const y = "swag";
-    const x = "yolo";
-    const total = `${x}${y}`;
-  }
-});
-
-app.get("/leak", (req, res) => {
-  console.log("leaking memory!!");
-  res.status(200).send("Leaking..");
-  leak();
-});
+setTimeout(() => {
+  process.exit(1);
+}, 20000);
