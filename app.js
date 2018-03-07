d5fcbef98024a7a249a8c368208f52c507c454ce
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2018-03-07 13:16:35 +0100

    no deps

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var app = express();
+//var express = require('express');
+//var app = express();
 process.exit(1);
 
-app.get('/', function(req, res){
-  res.send('Hello, second version !');
-});
-
-//app.listen(process.env.PORT);
-
-setTimeout(() => {
-  process.exit(1);
-}, 20000);
+//app.get('/', function(req, res){
+//  res.send('Hello, second version !');
+//});
+//
+////app.listen(process.env.PORT);
+//
+//setTimeout(() => {
+//  process.exit(1);
+//}, 20000);
