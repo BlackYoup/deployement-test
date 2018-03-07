208a3a4fbb8276ea799d6ff5b0035e3cebcc29e1
Arnaud Lefebvre <a.lefebvre@outlook.fr>
2018-03-07 13:50:54 +0100

    working commit

--- app.js
+++ app.js
@@ -1,13 +1,8 @@
-//var express = require('express');
-//var app = express();
-process.exit(1);
+var express = require('express');
+var app = express();
 
-//app.get('/', function(req, res){
-//  res.send('Hello, second version !');
-//});
-//
-////app.listen(process.env.PORT);
-//
-//setTimeout(() => {
-//  process.exit(1);
-//}, 20000);
+app.get('/', function(req, res){
+  res.send('Hello, second version !');
+});
+
+app.listen(process.env.PORT);
